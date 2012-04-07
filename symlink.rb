ebc2647726aa70e42f7d8c92fa9ef1f32bf8ee61
Andreas From <andro.from@gmail.com>
2012-04-07 15:18:47 +0200

    script now moves existing files to .old instead of skipping them

--- symlink.rb
+++ symlink.rb
@@ -5,7 +5,7 @@ home = File.expand_path('~')
 Dir['*'].each do |file|
   next if file =~ /install/
   target = File.join(home, ".#{file}")
-  # File.rename(target, File.join(target, ".old") if File.exists?(target)
+  `mv #{target} #{target}.old` if File.exists?(target)
   `ln -s #{File.expand_path file} #{target}`
 end
 
